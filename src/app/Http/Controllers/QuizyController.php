6f9644bea22b1c267e96de5d8ed7a8b75c58e539
kazuki-naka <kazunaka.0077@gmail.com>
2022-07-29 19:49:46 +0900

    week38終了

--- src/app/Http/Controllers/QuizyController.php
+++ src/app/Http/Controllers/QuizyController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Choice;
+use App\Picture;
+use App\Prefecture;
+
 
 class QuizyController extends Controller
 {
     public function index($id){
-        $big_questions = DB::table('big_questions') -> where('id', $id) -> first();
-        $questions = DB::table('questions') -> where('big_question_id', $id) -> get();
-        $choices = DB::table('choices') -> get();
-        return view('quiz.index', ['big_questions' => $big_questions, 'questions' => $questions, 'choices' => $choices, 'id' => $id]);
+        $big_questions = Prefecture::where('id', $id) -> first();
+        $questions = Prefecture::with('getData') -> find($id) -> getData;
+        $choices_list = Picture::with('getChoice') -> where('big_question_id', $id) -> get();
+        return view('quiz.index', compact('big_questions', 'questions', 'choices_list', 'id'));
     }
 }
