8fc8b83bd900549bb60151fcfdca2995db388e2f
kazuki-naka <kazunaka.0077@gmail.com>
2022-08-25 09:40:35 +0900

    問題タイトルの順序変更をjsで実装した。具体的にはドラッグ&ドロップで変更可能にした

--- src/resources/views/quiz/main.blade.php
+++ src/resources/views/quiz/main.blade.php
@@ -5,18 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>地名クイズトップページ</title>
+    <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
 </head>
 <body>
-    <ul>
+    <ul class="drag-list">
         @foreach($big_questions as $big_question)
         <form method="post">
             @csrf
-            <a href="{{ '/quiz/' . $big_question->id }}"><input type="hidden" value="{{ $big_question->id }}" name="big_question_id">{{ $big_question->name }}の難読地名クイズ</a>
-            <input type="submit" value="削除" name="delete">
-            <input type="submit" onclick="location.href='./edit'" value="変更" name="update">
+            <li id="{{ 'item' . $loop->iteration }}" draggable="true">
+                <a href="{{ '/quiz/' . $big_question->id }}"><input type="hidden" value="{{ $big_question->id }}" name="big_question_id">{{ $big_question->name }}の難読地名クイズ</a>
+                <input type="submit" value="削除" name="delete">
+                <input type="submit" onclick="location.href='./edit'" value="変更" name="update">
+            </li>
         </form>
         @endforeach
     </ul>
     <a href="{{ url('/add') }}">追加</a>
+    <script src="{{ asset('/js/main.js') }}"></script>
 </body>
 </html>
